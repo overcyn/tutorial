05273195dee91d8e05f3c2e4036536011d60beed
Kevin Dang <overcyn@gmail.com>
2017-08-07 20:23:12 -0700

    Fix example project

--- tutorial.go
+++ tutorial.go
@@ -1,6 +1,8 @@
 package tutorial
 
 import (
+	"image/color"
+
 	"golang.org/x/image/colornames"
 	"gomatcha.io/bridge"
 	"gomatcha.io/matcha/layout/constraint"
@@ -15,18 +17,12 @@ type TutorialView struct {
 	// All components must implement the view.View interface. A basic implementation
 	// is provided by view.Embed.
 	view.Embed
+	TextColor color.Color
 }
 
 // This is our view's initializer.
-func New(ctx *view.Context, key string) *TutorialView {
-	// To prevent rebuilding the entire tree on every rerender, initializers will return
-	// the previous view if it already exists. Most views will contain this bit
-	// of boilerplate.
-	if v, ok := ctx.Prev(key).(*TutorialView); ok {
-		return v
-	}
-	// If there was no matching view, we create a new one.
-	return &TutorialView{Embed: ctx.NewEmbed(key)}
+func NewTutorialView() *TutorialView {
+	return &TutorialView{}
 }
 
 // Similar to React's render function. Views specify their properties and
@@ -36,26 +32,26 @@ func (v *TutorialView) Build(ctx *view.Context) view.Model {
 
 	// Get the textview for the given key (hellotext), either initializing it or fetching
 	// the previous one.
-	textv := textview.New(ctx, "hellotext")
-	textv.String = "Hello World"
-	textv.Style.SetTextColor(colornames.Red)
-	textv.Style.SetFont(text.Font{
+	child := textview.New()
+	child.String = "Hello World"
+	child.Style.SetTextColor(v.TextColor)
+	child.Style.SetFont(text.Font{
 		Family: "Helvetica Neue",
 		Face:   "Bold",
 		Size:   50,
 	})
-	textv.PaintStyle = &paint.Style{BackgroundColor: colornames.Blue}
+	child.PaintStyle = &paint.Style{BackgroundColor: colornames.Blue}
 
-	// Layout is primarily done using a constraints. More info can be
+	// Layout is primarily done using constraints. More info can be
 	// found in the matcha/layout/constraints docs.
-	l.Add(textv, func(s *constraint.Solver) {
+	l.Add(child, func(s *constraint.Solver) {
 		s.Top(20)
 		s.Left(0)
 	})
 
 	// Returns the view's children, layout, and styling.
 	return view.Model{
-		Children: []view.View{textv},
+		Children: []view.View{child},
 		Layouter: l,
 		Painter:  &paint.Style{BackgroundColor: colornames.Lightgray},
 	}
@@ -63,9 +59,11 @@ func (v *TutorialView) Build(ctx *view.Context) view.Model {
 
 func init() {
 	// Registers a function with the objc bridge. This function returns
-	// a view.Root, which can be display in MatchaViewController.
+	// a view.Root, which can be displayed in a MatchaViewController.
 	bridge.RegisterFunc("github.com/overcyn/tutorial New", func() *view.Root {
 		// Call the TutorialView initializer.
-		return view.NewRoot(New(nil, ""))
+		v := NewTutorialView()
+		v.TextColor = colornames.Red
+		return view.NewRoot(v)
 	})
 }
